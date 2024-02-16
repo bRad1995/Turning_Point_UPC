037c15aa082155c94d7f29a96d1d5709f28f59fb
bRad1995 <bradleejwilliams@yahoo.com>
2024-02-15 20:37:00 -0600

    Templates, urls and views created

--- turningpoint_upc/main_app/urls.py
+++ turningpoint_upc/main_app/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('our_story/', views.our_story, name='our_story_index'),
+    path('members/', views.staff_index, name='staff_index'),
+    path('events/', views.events_index, name='events_index'),
+    path('contact/', views.contact_us, name='contact_us'),
 ]
--- turningpoint_upc/main_app/views.py
+++ turningpoint_upc/main_app/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def our_story(request):
+    return render(request, 'our_story.html')
+
+def staff_index(request):
+    return render(request, 'staff_members/staff_index.html')
+
+def events_index(request):
+    return render(request, 'events/events_index.html')
+
+def contact_us(request):
+    return render(request, 'contact.html')
