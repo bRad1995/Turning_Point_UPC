1e8e5ac53d010aea2effa75f81964f75e0de6729
bRad1995 <bradleejwilliams@yahoo.com>
2024-02-17 17:57:28 -0600

    Events and Members Models and index pages created

--- turningpoint_upc/main_app/models.py
+++ turningpoint_upc/main_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 
+class StaffMember (models.Model):
+    name = models.CharField(max_length=250)
+    role = models.CharField(max_length=250)
+    about = models.TextField
+
+    def __def__(self):
+        return self.name
+    
+class Event (models.Model):
+    name = models.CharField(max_length=250)
+    date = models.DateField
+    time = models.TimeField
+    location = models.CharField(max_length=250)
+    description = models.TextField
+
+    def __def__(self):
+        return self.name
