1ac13514f0848e5259eb57b631a100f9a282f8a3
bRad1995 <bradleejwilliams@yahoo.com>
2024-03-02 15:58:55 -0600

    corrected allowed hosts again

--- turningpoint_upc/turningpoint_upc/settings.py
+++ turningpoint_upc/turningpoint_upc/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
